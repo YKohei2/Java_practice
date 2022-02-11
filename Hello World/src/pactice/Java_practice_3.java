7a12427679ec24d44a3aaa9a2618975aa2dbc277
YKohei2 <koheiku.222@gmail.com>
2022-02-12 04:08:02 +0900

    raisetech 第2回

---
+++ Hello World/src/pactice/Java_practice_3.java
@@ -0,0 +1,75 @@
+package pactice;
+
+import java.time.LocalDateTime;
+import java.time.DayOfWeek;
+
+public class Java_practice_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*-------------------------------------------*/	
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯します");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+/*-------------------------------------------*/			
+		boolean doorClose = true;
+		while (doorClose == false) {
+            System.out.println("ノックをします");
+            System.out.println("1分間待ちます");
+	    }
+/*-------------------------------------------*/			
+		LocalDateTime Date = LocalDateTime.now();
+		System.out.println("Date : "+Date);
+		
+		DayOfWeek DW = Date.getDayOfWeek();
+		System.out.println("曜日: "+DW);
+		
+		DayOfWeek dayafter3 = Date.plusYears(3).getDayOfWeek();
+		System.out.println("3年後の今日は" +Date.plusYears(3));
+		System.out.println("３年後の今日の曜日は" +dayafter3);
+
+/*-------------------------------------------*/			
+		for (int i = 0; i < 13; i++) {
+		 
+		if (i  < 10) {
+			System.out.println("Hello World");
+		} else {
+			System.out.println("Hello");
+		}
+	}
+/*-------------------------------------------*/			
+		String greeting;
+		greeting = "Hello World";
+		    System.out.println(greeting);  
+
+/*-------------------------------------------*/			
+		    
+		int sum = 0;
+		
+		for (int i = 1; i <= 10; i++) {
+	       	sum = sum + i;
+		
+         System.out.println(sum);	
+		}
+/*------------------------------------------*/
+         hello();
+         
+/*------------------------------------------*/
+         int num1 = 0;
+         int num2 = 1;
+         if (num1 < 5 || num2 < 20) {
+        	 System.out.println("引数が0なら偽");
+         }{
+        	 System.out.println("引数が1なら真");
+         }
+         	
+    }
+	public static void hello() {
+   	 System.out.println("こんにちは");
+    }
+	
+}
